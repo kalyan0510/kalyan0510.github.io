57ed4027b3c5d1ac64ff88730bab6bbc95c78693
Kalyan <gkalyan0510@gmail.com>
2019-09-18 15:56:27 +0530

    update

--- scripts/getstats.js
+++ scripts/getstats.js
@@ -1,22 +1,29 @@
-
-function ipLookUp () {
-  $.ajax('http://ip-api.com/json')
-  .then(
-      function success(response) {
-
-                    $.ajax({
+function putStatData (response) {
+                $.ajax({
                       url:"https://githubstats.000webhostapp.com/setstat.php", //the page containing php script
                       type: "post", //request type,
                       dataType: 'json',
                      data: {ip: response.query, inf: JSON.stringify(response), oth: response.city, page: cpage}
                    });
-          console.log('User\'s Location Data is ', cpage);
-          // console.log('User\'s Country', response.country);
-      },
+}
+function ipLookUp () {
+  fetch('http://ip-api.com/json')
+   .then( response => response.json() )
+   .then( data => putStatData(data) )
+  $.get('https://ipinfo.io')
+  .then(
+      // function success(response) {
 
-      function fail(data, status) {
-          // console.log('Request failed.  Returned status of',            status);
-      }
+      //               $.ajax({
+      //                 url:"https://githubstats.000webhostapp.com/setstat.php", //the page containing php script
+      //                 type: "post", //request type,
+      //                 dataType: 'json',
+      //                data: {ip: response.query, inf: JSON.stringify(response), oth: response.city, page: cpage}
+      //              });
+      // },
+
+      // function fail(data, status) {
+      // }
   );
 }
 ipLookUp()
