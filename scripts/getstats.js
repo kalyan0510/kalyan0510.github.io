73847d69097881d9d376c42c1911f7b12031462f
Kalyan G <gkalyan0510@gmail.com>
2021-11-08 05:40:45 -0500

    Update getstats.js

--- scripts/getstats.js
+++ scripts/getstats.js
@@ -1,29 +1,31 @@
-function putStatData (response) {
+function getStatData (response) {
                 $.ajax({
-                      url:"https://githubstats.000webhostapp.com/setstat.php", //the page containing php script
+                      url:"https://l1az6ozvxa.execute-api.us-east-1.amazonaws.com/alpha/helloworld", //the page containing php script
                       type: "post", //request type,
                       dataType: 'json',
-                     data: {ip: response.ip, jsondata: JSON.stringify(response), city: response.city, page: cpage, region:response.region, org:response.org}
+                      contentType: "application/json",
+                      // headers: {'ip': ip, 'page':cpage},
+                     data: JSON.stringify({ip: response.ip, jsondata: JSON.stringify(response), city: response.city, page: cpage, region:response.region, org:response.org})
                    });
 }
 function ipLookUp () {
   fetch('https://ipapi.co/json')
    .then( response => response.json() )
-   .then( data => putStatData(data) )
-  $.get('https://ipinfo.io')
-  .then(
-      // function success(response) {
+   .then( data => getStatData(data) )
+  // $.get('https://ipinfo.io')
+  // .then(
+  //     // function success(response) {
 
-      //               $.ajax({
-      //                 url:"https://githubstats.000webhostapp.com/setstat.php", //the page containing php script
-      //                 type: "post", //request type,
-      //                 dataType: 'json',
-      //                data: {ip: response.query, inf: JSON.stringify(response), oth: response.city, page: cpage}
-      //              });
-      // },
+  //     //               $.ajax({
+  //     //                 url:"https://githubstats.000webhostapp.com/setstat.php", //the page containing php script
+  //     //                 type: "post", //request type,
+  //     //                 dataType: 'json',
+  //     //                data: {ip: response.query, inf: JSON.stringify(response), oth: response.city, page: cpage}
+  //     //              });
+  //     // },
 
-      // function fail(data, status) {
-      // }
-  );
+  //     // function fail(data, status) {
+  //     // }
+  // );
 }
 ipLookUp()
