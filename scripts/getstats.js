5d8161e242b1b4ad20f8464e41d6cd2c86839fee
Kalyan G <gkalyan0510@gmail.com>
2022-03-07 15:16:56 -0500

    Update getstats.js

--- scripts/getstats.js
+++ scripts/getstats.js
@@ -12,20 +12,5 @@ function ipLookUp () {
   fetch('https://ipapi.co/json')
    .then( response => response.json() )
    .then( data => getStatData(data) )
-  // $.get('https://ipinfo.io')
-  // .then(
-  //     // function success(response) {
-
-  //     //               $.ajax({
-  //     //                 url:"https://githubstats.000webhostapp.com/setstat.php", //the page containing php script
-  //     //                 type: "post", //request type,
-  //     //                 dataType: 'json',
-  //     //                data: {ip: response.query, inf: JSON.stringify(response), oth: response.city, page: cpage}
-  //     //              });
-  //     // },
-
-  //     // function fail(data, status) {
-  //     // }
-  // );
 }
 ipLookUp()
